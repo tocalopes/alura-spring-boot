39f28a9e4f11dfcbd2c6bcd628d1fa7dfeca4783
gabriel <gabriellopes112009@gmail.com>
2021-06-06 19:27:47 -0300

    Criação de DTO para topicos e implementação no controller

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,24 +1,23 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    @ResponseBody
-    public List<Topico> lista(){
+    public List<TopicoDto> lista(){
         Topico topico = new Topico(
-    "Duvida",
-"Duvida com Spring",
+    "Dúvida",
+"Duvida com Spring chablau",
           new Curso("Spring","Programação"));
-        return Arrays.asList(topico,topico,topico);
+        return TopicoDto.converter(Arrays.asList(topico,topico,topico));
     }
 }
