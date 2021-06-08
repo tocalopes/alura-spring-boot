2d02f46649679ce5478990aa9228e827d6ae4575
gabriel <gabriellopes112009@gmail.com>
2021-06-08 00:58:11 -0300

    Implementando autenticação com spring security, geração de hash para senhas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$RZdVdVez.A.JDmDQL1yspecbJknccORUbu1Qe4CvWyHi6z0ASS.hW');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
