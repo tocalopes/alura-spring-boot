4e68999b000a1597ebbd3f1afd8f7b538eba9308
gabriel <gabriellopes112009@gmail.com>
2021-06-08 00:45:51 -0300

    Configuração da entidade que será usada com usuário, e form de autenticação

---
+++ src/main/java/br/com/alura/forum/modelo/Perfil.java
@@ -0,0 +1,53 @@
+package br.com.alura.forum.modelo;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Perfil implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Perfil perfil = (Perfil) o;
+        return Objects.equals(id, perfil.id) && Objects.equals(nome, perfil.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.nome;
+    }
+}
